bdd0101c4bd1ee9e8bcd3c450b2d808fb457306e
laon <laon198@gmail.com>
2021-05-28 17:51:45 +0900

    time tester

--- Sort/BinaryInsertionSort.java
+++ Sort/BinaryInsertionSort.java
@@ -9,18 +9,6 @@
 //    }
 //
 //    private static int binarySearchIdx(Comparable[] arr, int start, int end, Comparable value) {
-//        if(start==end){ //start가 0일때
-//            if(value.compareTo(arr[start])<0) return 0;
-//            else return 1;
-//        }
-//
-//        int curIdx = (start+end)/2;
-//        while(true){
-//            if(value.compareTo(arr[curIdx])>0){
-//                curIdx=(curIdx+end)/2;
-//            }
-//            else if(value.compareTo(arr[curIdx]<0)) curIdx=(start+curIdx)/2;
-//        }
 //
 //    }
 //}
