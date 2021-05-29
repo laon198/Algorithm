3a02d87e6198ee761a3ddb529ee5b4202357b251
laon <laon198@gmail.com>
2021-05-30 01:49:20 +0900

    quickSortTest

--- test/QuickSortTest.java
+++ test/QuickSortTest.java
@@ -1,11 +1,10 @@
 package test;
 
-import Sort.BubbleSort;
 import Sort.QuickSort;
 import subject.DataGenerator;
 
 
-public class BubbleSortTest {
+public class QuickSortTest {
     private final static int[] SIZE_ARR = {
             5000, 10000, 25000, 50000, 100000
     };
@@ -18,12 +17,12 @@ public class BubbleSortTest {
 
         System.out.println("오름차순 정수 배열");
         for(int size : SIZE_ARR){
-            testRandomIntTime(size);
+            testIncreseIntTime(size);
         }
 
-        System.out.println("오름차순 정수 배열");
+        System.out.println("내림차순 정수 배열");
         for(int size : SIZE_ARR){
-            testRandomIntTime(size);
+            testDecreseIntTime(size);
         }
     }
 
@@ -31,20 +30,20 @@ public class BubbleSortTest {
         long timeB = System.currentTimeMillis();
         QuickSort.sort(DataGenerator.getRandomIntArr(size));
         long timeA = System.currentTimeMillis();
-        System.out.println("time : "+(double)(timeA-timeB)/1000);
+        System.out.println(size+"개 "+"time : "+(double)(timeA-timeB)/1000);
     }
 
     private static void testIncreseIntTime(int size){
         long timeB = System.currentTimeMillis();
         QuickSort.sort(DataGenerator.getIncreseArr(size));
         long timeA = System.currentTimeMillis();
-        System.out.println("time : "+(double)(timeA-timeB)/1000);
+        System.out.println(size+"개 "+"time : "+(double)(timeA-timeB)/1000);
     }
 
     private static void testDecreseIntTime(int size){
         long timeB = System.currentTimeMillis();
         QuickSort.sort(DataGenerator.getDecreseArr(size));
         long timeA = System.currentTimeMillis();
-        System.out.println("time : "+(double)(timeA-timeB)/1000);
+        System.out.println(size+"개 "+"time : "+(double)(timeA-timeB)/1000);
     }
 }
