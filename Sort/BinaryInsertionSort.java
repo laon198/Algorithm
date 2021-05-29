4d74a0257b7d459adab488c9ca7a0efdbad117bc
laon <laon198@gmail.com>
2021-05-29 19:43:36 +0900

    binartInsertion

--- Sort/BinaryInsertionSort.java
+++ Sort/BinaryInsertionSort.java
@@ -1,16 +1,53 @@
-//package Sort;
-//
-//public class BinaryInsertionSort {
-//	
-//    public static void sort(Comparable[] arr){
-//    	
-//        }
-//        
-//
-//    private static int binarySearchIdx(Comparable[] arr, int start, int end, Comparable value) {
-//
-//    
-//    	}
-//
-//	}
-//}
+package Sort;
+
+public class BinaryInsertionSort {
+    public static void sort(Comparable[] arr){
+        for(int i=1; i<arr.length; i++){
+            int insertIdx = binarySearchIdx(arr, 0, i-1, arr[i]);
+            shift(arr, insertIdx, i);
+        }
+    }
+
+    private static void shift(Comparable[] arr, int insertIdx, int valueIdx) {
+        Comparable tmp = arr[valueIdx];
+
+        for(int i=valueIdx-1; i>=insertIdx; i--){
+            arr[i+1] = arr[i];
+        }
+
+        arr[insertIdx] = tmp;
+    }
+
+
+    private static int binarySearchIdx(Comparable[] arr, int start, int end, Comparable value) {
+        int mid = 0;
+
+        while((end-start)>1){
+            mid = (end+start)/2;
+
+            if(value.compareTo(arr[mid])>0){
+                start = mid;
+            }else if(value.compareTo(arr[mid])<0){
+                end = mid;
+            }else if(value.compareTo(arr[mid])==0){
+                return mid+1;
+            }
+        }
+
+        if(mid==end){
+            if(value.compareTo(arr[start])<0){
+                return start==0 ? start : start-1;
+            }
+            else{
+                return start+1;
+            }
+        }else{
+            if(value.compareTo(arr[end])>=0){
+                return arr.length-1==end ? end : end+1;
+            }
+            else{
+                return end-1;
+            }
+        }
+	}
+}
--- Sort/Test.java
+++ Sort/Test.java
@@ -6,26 +6,20 @@ import subject.*;
 
 public class Test {
 	public static void main(String args[]) {
-		
+
 		IntegerGenerator.setSeed(10);
-		Integer[] intRanArr = IntegerGenerator.random(5000, 1000);
+		Integer[] intRanArr = IntegerGenerator.random(10000, 1000);
 		long timeB = System.currentTimeMillis();
-		BubbleSort.sort(intRanArr);
+		InsertionSort.sort(intRanArr);
 		long timeA = System.currentTimeMillis();
 		System.out.println("time : " + (double) (timeA - timeB) / 1000);
 
-		IntegerGenerator.setSeed(10);
-		Integer[] intInArr = IntegerGenerator.increase(5000, 1000);
-		timeB = System.currentTimeMillis();
-		BubbleSort.sort(intInArr);
-		timeA = System.currentTimeMillis();
-		System.out.println("time : " + (double) (timeA - timeB) / 1000);
-
-		IntegerGenerator.setSeed(10);
-		Integer[] intDeArr = IntegerGenerator.decrease(5000, 1000);
-		timeB = System.currentTimeMillis();
-		BubbleSort.sort(intDeArr);
-		timeA = System.currentTimeMillis();
-		System.out.println("time : " + (double) (timeA - timeB) / 1000);
+//
+//		IntegerGenerator.setSeed(10);
+//		Integer[] intDeArr = IntegerGenerator.decrease(5000, 1000);
+//		timeB = System.currentTimeMillis();
+//		BubbleSort.sort(intDeArr);
+//		timeA = System.currentTimeMillis();
+//		System.out.println("time : " + (double) (timeA - timeB) / 1000);
 	}
 }
