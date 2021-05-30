68b4a4d8c2da50405415172418c8bf5fdfc6ec37
laon <laon198@gmail.com>
2021-05-30 21:08:05 +0900

    HexRadixSort

---
+++ Sort/HexRadixSort.java
@@ -0,0 +1,45 @@
+package Sort;
+
+public class HexRadixSort {
+    public static void sort(Integer[] arr){
+        int max = getMax(arr);
+        for(int exp=1; max/exp>0; exp*=16){
+            countSort(arr, exp);
+        }
+    }
+
+    private static void countSort(Integer[] arr, int exp) {
+        int[] output = new int[arr.length];
+        int[] count = new int[16];
+
+        for(int i=0; i<arr.length; i++){
+            count[(arr[i]/exp)%0x10]++;
+        }
+
+        for(int i=1; i<16; i++) {
+            count[i] += count[i-1];
+        }
+
+        for(int i=arr.length-1; i>=0; i--){
+            output[count[(arr[i]/exp)%0x10]-1] = arr[i];
+            count[(arr[i]/exp)%0x10]--;
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = output[i];
+        }
+
+
+    }
+
+    private static int getMax(Integer[] data) {
+        int mx = data[0];
+        for(int i=1; i<data.length; i++) {
+            if(data[i] > mx) {
+                mx = data[i];
+            }
+        }
+        return mx;
+    }
+
+}
--- Sort/Test.java
+++ Sort/Test.java
@@ -1,19 +1,17 @@
 package Sort;
 
-import java.util.Arrays;
-
 import subject.*;
 
 public class Test {
 	public static void main(String args[]) {
 
 		IntegerGenerator.setSeed(10);
-		Integer[] intRanArr = IntegerGenerator.random(15, 100);
+		Integer[] intRanArr = IntegerGenerator.random(100000, 100);
 		long timeB = System.currentTimeMillis();
-		IterativeMergeSort.sort(intRanArr);
+		HexRadixSort.sort(intRanArr);
 		long timeA = System.currentTimeMillis();
 		System.out.println("time : " + (double) (timeA - timeB) / 1000);
-		System.out.println(Arrays.toString(intRanArr));
+
 
 //
 //		IntegerGenerator.setSeed(10);
